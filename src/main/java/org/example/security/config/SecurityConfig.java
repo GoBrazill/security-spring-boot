894088aaff8d6d0dedba79fd4471ccc6a82171d5
GoBrazill <vinicius.daraujos26@gmail.com>
2026-04-22 10:58:09 +0000

    feat: change from httpBasic to jwt filter

--- src/main/java/org/example/security/config/SecurityConfig.java
+++ src/main/java/org/example/security/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -16,20 +17,24 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    private final JwtFilter jwtFilter;
+
+    public SecurityConfig(JwtFilter jwtFilter) {
+        this.jwtFilter = jwtFilter;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http){
 
-        http
+         return http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
                 .requestMatchers("/user", "/login").permitAll()
                 .requestMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 )
-        .httpBasic(withDefaults());
+        .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class).build();
 
-
-        return http.build();
     }
 
     @Bean
